4b2e319b2100854312a5da9e1dc95eac734a3eae
Manasa H M <manasahm2005@gmail.com>
2026-01-31 15:05:29 +0530

    image

--- app.py
+++ app.py
@@ -16,4 +16,5 @@ def abc():
     return render_template("abc.html")
 
 if __name__ == "__main__":
-    app.run(debug=True,host="[IP_ADDRESS]",port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0",port=port,debug=True)
